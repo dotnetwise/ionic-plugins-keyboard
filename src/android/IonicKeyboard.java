cacff0d8272d05b5673e244be3608167f104a043
Laurenţiu Macovei <laurentiu.macovei@dotnetwise.com>
2015-04-08 17:26:43 +0300

    exit full screen mode

--- src/android/IonicKeyboard.java
+++ src/android/IonicKeyboard.java
@@ -136,7 +136,10 @@ public class IonicKeyboard extends CordovaPlugin{
         return false;  // Returning false results in a "MethodNotFound" error.
     }
 	private void goImmersive(Window window) {
-		if (Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) {
+		if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR1) {
+			getWindow().setFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN, 
+								 WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN)
+		} else if (Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) {
 			window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                     WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		}
@@ -150,7 +153,24 @@ public class IonicKeyboard extends CordovaPlugin{
 		}
     }
 	private void goNonImmersive(Window window) {
-		if (Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) {
+		if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR1) {
+			getWindow().setFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN, 
+								 WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN)
+		} else if (Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) {
+			window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		}
+	    else {
+			window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+		}
+    }
+	private void goFullScreen(Window window) {
+		if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR1) {
+			getWindow().setFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN, 
+								 WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN)
+		} else if (Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) {
 			window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                     WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		}
@@ -159,18 +179,21 @@ public class IonicKeyboard extends CordovaPlugin{
                     | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                     | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                     | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN);
 		}
-    }
-	private void goFullScreen(Window window) {
-		window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		
     }
 	private void goNonFullScreen(Window window) {
-		window.clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
-		
+		if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR1) {
+			getWindow().setFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN, 
+								 WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN)
+		} else if (Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) {
+			window.setFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+		}
+	    else {
+			window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+		}
     }
 }
 
